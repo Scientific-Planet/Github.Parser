27977f014096e54ed1ae8f62dce9337dd57caad4
Flippie Coetser <flippie.coetser@gmail.com>
2016-11-10 19:49:46 +0000

    update

--- source/entities/parser.ts
+++ source/entities/parser.ts
@@ -1,47 +1,16 @@
-export class Markdown {
-    public static syntax = {
-        Code: "`X`",
-        Emphasis: {
-            Bold: "**X** or __X__",
-            BoldAndItallics: "**_X_**",
-            Italics: "*X* or _X_",
-            Strikethrough: "~~X~~",
-        },
-        Headers: {
-            H1: /#{1}\s/,
-            H2: /#{2}\s/,
-            H3: /#{3}\s/,
-            H4: /#{4}\s/,
-            H5: /#{5}\s/,
-            H6: /#{6}\s/,
-        },
-        Horizontal: {
-            Line: "*** or --- or ___",
-        },
-        Images: "![alt text](url.png)",
-        Links: {
-            Inline: "[title](url)",
-            Mention: "@",
-        },
-        Lists: {
-            Indent: "...",
-            Ordered: "1.",
-            OrderedSub: "..1.",
-            Task: "- [ ]",
-            UnOrdered: "* or - or +",
-            UnOrderedSub: "⋅⋅*",
-        },
-        Quotes: {
-            Block: ">",
-        },
-    };
+/// <reference path="../../typings/index.d.ts" />
+import * as peg from "pegjs";
+import * as fs from "fs";
+import * as path from "path";
 
-}
 export class Parser {
-    public static config = /\r\n/g;
-    public output;
-    constructor(public issue) { }
-    public parse() {
-        return this.issue.split(Parser.config);
+    constructor(public issue) {}
+    private static _loadFile(filePath: string) {
+        return fs.readFileSync(path.join(__dirname, filePath), "utf8");
     }
+    private _parser = peg.generate(Parser._loadFile("../grammer/markdown"));
+    public parse() {
+        return this._parser.parse(Parser._loadFile("../markdown/dev.md"));
+    }
+
 }
